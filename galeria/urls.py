df9bccf6d4fcdce54b0d372bcb708ee15ae017d3
igor belo <igorbello170@gmail.com>
2022-11-24 22:00:54 -0300

    adicionando nova imagens_4

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from galeria.views import carinanebula, smacs, index
+from galeria.views import carinanebula, smacs, index, stephan
 
 urlpatterns = [
     path ('', index, name='index'),
     path('carinanebula/', carinanebula, name='carinanebula'),
-    path('smacs/', smacs, name='smacs' )
+    path('smacs/', smacs, name='smacs' ),
+    path('stephan/', stephan, name ='stephan')
 ]
